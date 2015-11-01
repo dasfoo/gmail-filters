d8fdcb0e1e6901dbb71577d8676f01eee08293bf
Katarina Sheremet <kate.romanowskaya@gmail.com>
2015-11-01 21:38:06 +0100

    Move Title and Part of id in constructor

--- filter/smart_feed.go
+++ filter/smart_feed.go
@@ -11,7 +11,7 @@ func (feed *Feed) Personal(real_name []string, emails []string) {
 	message_filter_names := strings.Join(real_name, " OR ")
 	message_filter_mails := strings.Join(emails, " OR ")
 
-	entry := NewEntry("Mail Filter", "tag:dasfoo.filters,smartfilter:personal")
+	entry := NewEntry("personal")
 	entry.AddProperty("hasTheWord", message_filter_names+" OR  "+message_filter_mails)
 	entry.AddProperty("shouldStar", "true")
 
@@ -22,7 +22,7 @@ func (feed *Feed) Team(mails ...string) {
 	message_filter_mails := strings.Join(mails, " OR ")
 	log.Println(message_filter_mails)
 
-	entry := NewEntry("Mail Filter", "tag:dasfoo.filters,smartfilter:team")
+	entry := NewEntry("team")
 	entry.AddProperty("from", message_filter_mails)
 	entry.AddProperty("label", "Team")
 
@@ -32,7 +32,7 @@ func (feed *Feed) Team(mails ...string) {
 func (feed *Feed) Environment(listnames ...string) {
 	message_filter_lists := "list:(<" + strings.Join(listnames, "> OR <") + ">)"
 
-	entry := NewEntry("Mail Filter", "tag:dasfoo.filters,smartfilter:environment")
+	entry := NewEntry("environment")
 	entry.AddProperty("hasTheWord", message_filter_lists)
 	entry.AddProperty("label", "Environment")
 	entry.AddProperty("shouldArchive", "true")
@@ -43,7 +43,7 @@ func (feed *Feed) Environment(listnames ...string) {
 func (feed *Feed) Entertainment(listnames ...string) {
 	message_filter_lists := "list:(<" + strings.Join(listnames, "> OR <") + ">)"
 
-	entry := NewEntry("Mail Filter", "tag:dasfoo.filters,smartfilter:entertainment")
+	entry := NewEntry("entertainment")
 	entry.AddProperty("hasTheWord", message_filter_lists)
 	entry.AddProperty("label", "Entertainment")
 	entry.AddProperty("shouldArchive", "true")
@@ -54,7 +54,7 @@ func (feed *Feed) Entertainment(listnames ...string) {
 func (feed *Feed) Project(projectlabelname, projectname, interesting_review_string string, project_not_intresting bool) {
 	project_messages := fmt.Sprintf("list:(<%[1]s-users> OR <%[1]s-team> OR <%[1]s-sre> OR <%[1]s-eng>)", projectname)
 
-	project_message_entry := NewEntry("Mail Filter", "tag:dasfoo.filters,smartfilter:project_message"+strconv.Itoa(len(feed.Entries)))
+	project_message_entry := NewEntry("project_message" + strconv.Itoa(len(feed.Entries)))
 	project_message_entry.AddProperty("hasTheWord", project_messages)
 	project_message_entry.AddProperty("label", projectlabelname)
 	if project_not_intresting {
@@ -64,14 +64,14 @@ func (feed *Feed) Project(projectlabelname, projectname, interesting_review_stri
 
 	project_reviews_messages := fmt.Sprintf("list:(<%[1]s-reviews>) OR to:%[1]s+reviews", projectname)
 
-	project_review_entry := NewEntry("Mail Filter", "tag:dasfoo.filters,smartfilter:project_review"+strconv.Itoa(len(feed.Entries)))
+	project_review_entry := NewEntry("project_review" + strconv.Itoa(len(feed.Entries)))
 	project_review_entry.AddProperty("hasTheWord", project_reviews_messages)
 	project_review_entry.AddProperty("shouldArchive", "true")
 
 	feed.AddEntry(project_review_entry)
 
 	if interesting_review_string != "" {
-		project_interesting_entry := NewEntry("Mail Filter", "tag:dasfoo.filters,smartfilter:project_intresting"+strconv.Itoa(len(feed.Entries)))
+		project_interesting_entry := NewEntry("project_intresting" + strconv.Itoa(len(feed.Entries)))
 		project_interesting_entry.AddProperty("hasTheWord", project_reviews_messages+" AND "+interesting_review_string)
 		project_interesting_entry.AddProperty("label", projectlabelname)
 
@@ -80,7 +80,7 @@ func (feed *Feed) Project(projectlabelname, projectname, interesting_review_stri
 
 	project_alerts_messages := fmt.Sprintf("list:(<%[1]s-alerts> OR <%[1]s-notifications>) OR to:(%[1]s+alerts OR %[1]s+notifications)", projectname)
 
-	project_alerts_entry := NewEntry("Mail Filter", "tag:dasfoo.filters,smartfilter:project_alerts"+strconv.Itoa(len(feed.Entries)))
+	project_alerts_entry := NewEntry("project_alerts" + strconv.Itoa(len(feed.Entries)))
 	project_alerts_entry.AddProperty("to", project_alerts_messages)
 	if !project_not_intresting {
 		project_alerts_entry.AddProperty("label", projectlabelname)
@@ -93,7 +93,7 @@ func (feed *Feed) Project(projectlabelname, projectname, interesting_review_stri
 func (feed *Feed) Service(servicename string, listnames ...string) {
 	message_filter_lists := "list:(<" + strings.Join(listnames, "> OR <") + ">)"
 
-	entry := NewEntry("Mail Filter", "tag:dasfoo.filters,smartfilter:service"+strconv.Itoa(len(feed.Entries)))
+	entry := NewEntry("ervice" + strconv.Itoa(len(feed.Entries)))
 	entry.AddProperty("hasTheWord", message_filter_lists)
 	entry.AddProperty("label", servicename)
 
@@ -104,7 +104,7 @@ func (feed *Feed) AutomatedSystem(systemname string, mails ...string) {
 	message_filter_mails := strings.Join(mails, " OR ")
 	log.Println(message_filter_mails)
 
-	entry := NewEntry("Mail Filter", "tag:dasfoo.filters,smartfilter:automated"+strconv.Itoa(len(feed.Entries)))
+	entry := NewEntry("automated" + strconv.Itoa(len(feed.Entries)))
 	entry.AddProperty("from", message_filter_mails)
 	entry.AddProperty("label", systemname)
 	entry.AddProperty("shouldArchive", "true")
